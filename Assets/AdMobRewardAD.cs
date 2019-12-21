9e11293d36b0a5428c9ad4a9d2d9259cc2035bb9
Xtian2108 <christian_2108@live.com>
2019-12-21 13:32:37 -0500

    Agregado nuevos niveles y sus respectivos iconos

---
+++ Assets/AdMobRewardAD.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GoogleMobileAds.Api;
+using UnityEngine.UI;
+using System;
+
+namespace BizzyBeeGames.WordGame
+{
+    public class AdMobRewardAD : MonoBehaviour
+    {
+
+        private BannerView banneView;
+
+        private RewardBasedVideoAd rewardedVideo;
+
+        public string androidRewardedID;
+
+        // Use this for initialization
+        void Start()
+        {
+            MobileAds.Initialize(androidRewardedID);
+            rewardedVideo = RewardBasedVideoAd.Instance;
+            rewardedVideo.OnAdRewarded += HandleRewardBasedVideoRewarded;
+            rewardedVideo.OnAdClosed += HandleRewardBasedVideoClosed;
+            RequestRewardVideo();
+        }
+
+        public void ShowRewardedVideo()
+        {
+            if (rewardedVideo.IsLoaded())
+            {
+                rewardedVideo.Show();
+            }
+        }
+
+        public void RequestRewardVideo()
+        {
+            rewardedVideo.LoadAd(CreateNewRequest(), androidRewardedID);
+        }
+
+        public AdRequest CreateNewRequest()
+        {
+            return new AdRequest.Builder().Build();
+        }
+
+        private void HandleRewardBasedVideoRewarded(object sender, Reward args)
+        {
+            double amount = args.Amount;
+            GameManager.Instance.AddHint();
+        }
+
+        public void HandleRewardBasedVideoClosed(object sender, EventArgs args)
+        {
+            RequestRewardVideo();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
